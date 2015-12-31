4826930e6b1364ca28249ad810af6a25cb722467
Brody <rufengch@gmail.com>
2015-12-30 22:28:36 -0800

    delete ifDisplay property in markerData

--- js/app.js
+++ js/app.js
@@ -4,33 +4,27 @@ var myLatLng = {lat: 37.703383, lng: -122.472916};
 var markerData = [
 {
     name: 'Boulevard Cafe',
-    position: {lat: 37.704003, lng: -122.475030},
-    ifDisplay: true
+    position: {lat: 37.704003, lng: -122.475030}
 },
 {
     name: '76',
-    position: {lat: 37.703646, lng: -122.475802},
-    ifDisplay: true
+    position: {lat: 37.703646, lng: -122.475802}
 },
 {
     name: 'Kome Buffet',
-    position: {lat: 37.702610, lng: -122.470373},
-    ifDisplay: true
+    position: {lat: 37.702610, lng: -122.470373}
 },
 {
     name: 'Subway',
-    position: {lat: 37.701400, lng: -122.470252},
-    ifDisplay: true
+    position: {lat: 37.701400, lng: -122.470252}
 },
 {
     name: 'Westlake Coffee Shop',
-    position: {lat: 37.702359, lng: -122.470520},
-    ifDisplay: true
+    position: {lat: 37.702359, lng: -122.470520}
 },
 {
     name: 'Century 20 Daly City and XD',
-    position: {lat: 37.702155, lng: -122.470354},
-    ifDisplay: true
+    position: {lat: 37.702155, lng: -122.470354}
 }
 ];
 var markerData2 = [];
