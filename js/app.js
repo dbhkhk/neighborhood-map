66252d2cfd2c5d0dfcb2125a559710a3973430a0
Brody <rufengch@gmail.com>
2016-01-07 15:55:16 -0800

    add yelpID for markerData

--- js/app.js
+++ js/app.js
@@ -5,27 +5,28 @@ var myLatLng = {lat: 37.703383, lng: -122.472916};
 var markerData = [
 {
     name: 'Boulevard Cafe',
-    position: {lat: 37.704003, lng: -122.475030}
+    position: {lat: 37.704003, lng: -122.475030},
+    yelpID: 'boulevard-cafe-daly-city'
 },
 {
     name: '76',
-    position: {lat: 37.703646, lng: -122.475802}
+    position: {lat: 37.703646, lng: -122.475802},
+    yelpID: 'mayfair-76-daly-city'
 },
 {
     name: 'Kome Buffet',
-    position: {lat: 37.702610, lng: -122.470373}
-},
-{
-    name: 'Subway',
-    position: {lat: 37.701400, lng: -122.470252}
+    position: {lat: 37.702610, lng: -122.470373},
+    yelpID: 'kome-japanese-seafood-and-grill-buffet-daly-city'
 },
 {
     name: 'Westlake Coffee Shop',
-    position: {lat: 37.702359, lng: -122.470520}
+    position: {lat: 37.702359, lng: -122.470520},
+    yelpID: 'westlake-coffee-shop-daly-city-2'
 },
 {
     name: 'Century 20 Daly City and XD',
-    position: {lat: 37.702155, lng: -122.470354}
+    position: {lat: 37.702155, lng: -122.470354},
+    yelpID: 'century-20-theatre-daly-city'
 }
 ];
 var markerData2 = [];
@@ -35,6 +36,14 @@ var populateMarkerData2 = function() {
     }
 };
 populateMarkerData2();
+
+// use Yelp API
+var yelpData = [];
+var yelpURLWithKeys = yelpURL + '/?' + 'oauth_consumer_key=' + parameters.oauth_consumer_key +
+    '&oauth_token=' + parameters.oauth_token + '&oauth_signature_method=' + parameters.oauth_signature_method +
+    '&oauth_signature=' + encodedSignature + '&oauth_timestamp=' + parameters.oauth_timestamp +
+    '&oauth_nonce=' + parameters.oauth_nonce;
+
 
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
